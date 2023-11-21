11c09664d1e54303492c04a78242f0ab6a9d5aaa
henrycheung1020 <146144664+henrycheung1020@users.noreply.github.com>
2023-11-21 21:21:56 +0000

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  skip_before_action(:authenticate_user!, { :only => [:index] })
   def home 
     redirect_to("/users")
   end
 
   def index 
-
+    
     render({ :template => "users/index" })
   end
 end
