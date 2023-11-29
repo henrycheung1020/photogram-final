c851b79b8e9b286d60f6d02f000c4d1abe642fb2
henrycheung1020 <146144664+henrycheung1020@users.noreply.github.com>
2023-11-29 00:16:25 +0000

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  
   skip_forgery_protection
+  before_action :authenticate_user!
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :avatar_url])
+
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:avatar_url])
+  end
 end
